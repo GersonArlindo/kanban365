03eab2efa2c4d2f56ce7e629ea917cef2a06b752
GersonArlindo <gerson50039@gmail.com>
2024-06-07 13:35:30 -0600

    Agregando cors al backend

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,7 @@ dotenv.config();
 
 // Create Express server
 const app = express();
-app.use(cors({
-    origin: 'https://kanban365.ez-marketing-us.com', // Cambia esto por el dominio de tu frontend
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-  }));
+app.use(cors())
 
 // Express configuration
 app.set("port", process.env.PORT ?? 3000);
